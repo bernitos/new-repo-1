e0bef9491936d1c8994315e7d3c5aa913e289d19
Your Name <you@example.com>
2024-01-28 20:18:06 +0100

    new-repo

--- utilities/index.js
+++ utilities/index.js
@@ -94,4 +94,20 @@ Util.buildClassificationGrid = async function(data){
 
 
 
+
+
+
+
+
+
+/* ****************************************
+ * Middleware For Handling Errors
+ * Wrap other function in this for 
+ * General Error Handling
+ **************************************** */
+Util.handleErrors = fn => (req, res, next) => Promise.resolve(fn(req, res, next)).catch(next)
+Ensure there are no warnings or errors.
+
+
+
 module.exports = Util
